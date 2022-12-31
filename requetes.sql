7aeb4518171bf8614d27c016d74c1cf81ab9ec2d
MCouteyen <43321160+Ambremari@users.noreply.github.com>
2022-12-31 15:58:48 +0100

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,1 +1,18 @@
+/* Liste des produits de type "Bien-être" avec leur quantité disponible en stock */
+SELECT NomProd, QuantiteStock
+FROM Produits
+WHERE LibelleType = 'Bien-être';
+
+/*Liste des clients et nombre de commandes enregistrées à leur nom */
+
+/*Donner le nombre de bons d'achat émis, le montant total de ces bons. */
+
+/* Donner le nombre de bons d'achat utilisés ainsi que le montant total de ces bons.*/
+
+/* Facture */ 
+/* Pour la commande n° ???, donner les coordonées du client */ 
+
+/* Pour la commande n°, lister les produits commandés, la quantité voulue pour chaque produit, leur prix hors-taxe et le prix TTC */
+
+/* Pour la commande n°, donner le numéro de la facture, la date de la facture, le prix total de la commande, le prix des frais de port ainsi que l'adresse du point relais */  
 
