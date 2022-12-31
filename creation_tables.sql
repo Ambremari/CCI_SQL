4f97ac3f3152ccf99f1202c04e957b21607db81b
MCouteyen <43321160+Ambremari@users.noreply.github.com>
2022-12-31 15:35:41 +0100

    Update creation_tables.sql
    Ajout ; manquant 
    et Char Varying

--- creation_tables.sql
+++ creation_tables.sql
@@ -13,11 +13,11 @@ DROP TABLE CodesPostaux;
 CREATE TABLE CodesPostaux(
   CP decimal(5)
 CONSTRAINT PK_CodesPostaux PRIMARY KEY,
-  Ville Char Varying (20) 
+  Ville Char Varying(20) 
 );
 
 CREATE TABLE FraisDePort(
-  PaysRel Char Varying (20)
+  PaysRel Char Varying(20)
 CONSTRAINT PK_FraisDePort PRIMARY KEY,
   FdP decimal(2)
 );
@@ -25,48 +25,48 @@ CONSTRAINT PK_FraisDePort PRIMARY KEY,
 CREATE TABLE Conseillers(
   IdCons decimal(8)
 CONSTRAINT PK_Conseillers PRIMARY KEY,
-  NomCons Char Var(20) NOT NULL,
-  PrenomCons Char Var(20) NOT NULL
-)
+  NomCons Char Varying(20) NOT NULL,
+  PrenomCons Char Varying(20) NOT NULL
+);
   
 CREATE TABLE Types(
-  LibelleType Char Var(20)
+  LibelleType Char Varying(20)
 CONSTRAINT PK_Types PRIMARY KEY,
   Taxe decimal(3,2)
-CONSTRAINT Dom_Taxe CHECK (Taxe>0 AND Taxe<1)
-)
+CONSTRAINT Dom_Taxe CHECK (Taxe > 0 AND Taxe < 1)
+);
 
 CREATE TABLE Produits(
   IdProd decimal(8)
 CONSTRAINT PK_Produits PRIMARY KEY,
-  NomProd Char Var(100) NOT NULL,
+  NomProd Char Varying(100) NOT NULL,
   PrixHT decimal(4),
   QuantiteStock decimal(6),
-  LibelleType Char Var(20)
+  LibelleType Char Varying(20)
 CONSTRAINT FK_Prod_ref_Types REFERENCES Types(LibelleType),
-)
+);
 
 CREATE TABLE PointsRelais(
   IdRel decimal(8)
 CONSTRAINT PK_PointsRelais PRIMARY KEY,
-  NomRel Char Var(50) NOT NULL,
+  NomRel Char Varying(50) NOT NULL,
   NumRueRel decimal(4),
-  NomRueRel Char Var(50),
+  NomRueRel Char Varying(50),
   CPRel decimal(5)
 CONSTRAINT FK_Rel_ref_CodesPostaux REFERENCES CodesPostaux(CP),
-  PaysRel Char Var(20)
+  PaysRel Char Varying(20)
 CONSTRAINT FK_Rel_ref_FraisDePort REFERENCES FraisDePort(PaysRel),
-)
+);
 
 CREATE TABLE Clients(
   IdCl decimal(8)
 CONSTRAINT PK_Clients PRIMARY KEY,
-  NomCl Char Var(20) NOT NULL,
-  PrenomCl Char Var(20) NOT NULL,
+  NomCl Char Varying(20) NOT NULL,
+  PrenomCl Char Varying(20) NOT NULL,
   NumRueCl decimal(4),
-  NomRueCl Char Var(50),
-  PaysCl Char Var(20),
-  MailCl Char Var(50)
+  NomRueCl Char Varying(50),
+  PaysCl Char Varying(20),
+  MailCl Char Varying(50)
 CONSTRAINT Dom_MailCl CHECK (MailCl LIKE (".*@.*\..*")),
   TelCl Char(10),
 CONSTRAINT Dom_TelCl CHECK (TelCl LIKE("0{0,9}*"),
@@ -77,14 +77,14 @@ CONSTRAINT Dom_DateNaisCl CHECK (DateNaisCl > '1900-01-01'),
 CONSTRAINT Dom_DateAdhCl CHECK (DateCmd > '2000-01-01'),
   CPCl decimal(5)
 CONSTRAINT FK_Cl_ref_CodesPostaux REFERENCES CodesPostaux(CP),
-)
+);
 
 CREATE TABLE Commandes(
   NumCmd decimal(8)
 CONSTRAINT PK_Commandes PRIMARY KEY,
   DateCmd Date
 CONSTRAINT Dom_DateCmd CHECK (DateCmd > '2000-01-01')
-  EtatCmd Char Var(30) NOT NULL DEFAULT "En attente de validation" CONSTRAINT Dom_EtatCmd CHECK (EtatCmd IN("En attente de validation", "Payée, en attente de préparation", "En préparation", "Expédiée", "En point relais", "Finalisée"), 
+  EtatCmd Char Varying(30) NOT NULL DEFAULT "En attente de validation" CONSTRAINT Dom_EtatCmd CHECK (EtatCmd IN("En attente de validation", "Payée, en attente de préparation", "En préparation", "Expédiée", "En point relais", "Finalisée"), 
   DateLiv Date
 CONSTRAINT Dom_DateLiv CHECK (DateLiv > DateCmd),
   PrixTotCmd decimal(4),
@@ -94,7 +94,7 @@ CONSTRAINT FK_Cmd_ref_Cl REFERENCES Clients(IdCl),
 CONSTRAINT FK_Cmd_ref_Cons REFERENCES Conseillers(IdCons),
   IdRel decimal(8)
 CONSTRAINT FK_Cmd_ref_Rel REFERENCES PointsRelais(IdRel),
-)
+);
 
 CREATE TABLE BonsAchats(
   NumBon decimal(8)
@@ -105,7 +105,7 @@ CONSTRAINT PK_BonsAchats PRIMARY KEY,
 CONSTRAINT FK_BonsAch_ref_Cl REFERENCES Clients(IdCl),
   NumCmd decimal(8)
 CONSTRAINT FK_BonsAch_ref_Cmd REFERENCES Commandes(NumCmd),
-)
+);
 
 CREATE TABLE Factures(
   NumFac decimal(8)
@@ -114,7 +114,7 @@ CONSTRAINT PK_Factures PRIMARY KEY,
 CONSTRAINT Dom_DateFac CHECK (DateFac > DateCmd),,
   NumCmd decimal(8)
 CONSTRAINT FK_Fac_ref_Cmd REFERENCES Commandes(NumCmd),
-)
+);
 
 CREATE TABLE CompositionCmd(
   NumCmd decimal(8)
@@ -123,7 +123,7 @@ CONSTRAINT FK_Compo_ref_Cmd REFERENCES Commandes(NumCmd),
 CONSTRAINT FK_Compo_ref_Prod REFERENCES Produits(IdProd),
   QuantiteVoulue decimal(3),
 CONSTRAINT PK_CompositionCmd PRIMARY KEY(NumCmd,IdProd)
-)
+);
 
 
 
