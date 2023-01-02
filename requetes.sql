7d2cfe829a4136cff8eb02076c6f08d242b8c66b
MCouteyen <43321160+Ambremari@users.noreply.github.com>
2023-01-02 12:00:17 +0100

    Ajout de 20 requêtes

--- requetes.sql
+++ requetes.sql
@@ -1,13 +1,39 @@
-/* Liste des produits de type "Bien-être" avec leur quantité disponible en stock */
+/* Donner la liste des produits de type "Bien-être" avec leur quantité disponible en stock */
 SELECT NomProd, QuantiteStock
 FROM Produits
 WHERE LibelleType = 'Bien-être';
 
-/*Liste des clients et nombre de commandes enregistrées à leur nom */
+/* Donner la liste des clients et nombre de commandes enregistrées à leur nom. */
 
-/*Donner le nombre de bons d'achat émis, le montant total de ces bons. */
+/* Lister les commandes "En attente de validation" avec le numéro et nom du client et le numéro et nom du conseiller choisi. */
 
-/* Donner le nombre de bons d'achat utilisés ainsi que le montant total de ces bons.*/
+/* Donner la liste des points relais localisés en Charente-Maritime (17) avec leur adresse complète. */
+
+/* Lister les clients participant au programme de fidélité */ 
+
+/* Donner le nom des clients faisant partie du programme de fidélité depuis plus d'1 an. */
+
+/* Donner le nombre de bons d'achat émis, le montant total de ces bons. */
+
+/* Donner le nombre de bons d'achat utilisés ainsi que le montant total de ces bons. */
+
+/* Donner le montant dépensé dans chaque catégorie de produit pour chaque client. */ 
+
+/* Pour les clients faisant partie du programme de fidélité, donner l'âge moyen des clients. */
+
+/* Pour le client n°, donner son âge, sa date de naissance et la catégorie de produit dans laquelle il a dépensé le plus d'argent. */
+
+/* Lister tous les produits de la marque "" */. 
+
+/* Lister les clients ayant dépensé de l'argent les 2 dernier mois, avec le montant dépensé. */ 
+
+/* Donner le montant du panier moyen. */
+
+/* Donner le nom des clients ayant dépensé au total plus de 200 euros. */
+
+/* Donner le produit le plus commandé et la quantité écoulée. */
+
+/* Lister les produits qui n'ont jamais été commandés. */ 
 
 /* Facture */ 
 /* Pour la commande n° ???, donner les coordonées du client */ 
