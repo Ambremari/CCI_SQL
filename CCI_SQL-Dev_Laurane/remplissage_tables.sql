b6351328e9ca1ed84bcbb99486eab661e0706a3a
LauraneInq <121088722+LauraneInq@users.noreply.github.com>
2023-01-13 18:47:17 +0100

    Correction des requêtes avec oracle
    Correction des requêtes avec oracle + modification des expressions régulières de tel et mail dans client + modification des num de commandes associé au bons d'achat + retirer 10 euros des commandes dont le client a utilisé un bons d'achat

---
+++ CCI_SQL-Dev_Laurane/remplissage_tables.sql
@@ -0,0 +1,789 @@
+INSERT INTO Types(LibelleType, Taxe)
+	VALUES ('Cosmétique', 0.2);
+	
+INSERT INTO Types(LibelleType, Taxe)
+	VALUES ('Bien-être', 0.15);
+
+INSERT INTO Types(LibelleType, Taxe)
+	VALUES ('Entretien', 0.2);
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345671, 
+			'Huile végétale de Jojoba bio',
+			5.99, 
+			15,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345672, 
+			'Huile végétale d Avocat 100% origine naturelle',
+			4.50, 
+			20,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345673, 
+			'Huile pure Argan Bio - Grand format',
+			13.80, 
+			5,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345674, 
+			'Huile pure Argan Bio',
+			7.20, 
+			18,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345675, 
+			'Huile Végétale Coco Bio',
+			3.20, 
+			40,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345676, 
+			'Huile Végétale Ricin Bio',
+			8.10, 
+			30,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345677, 
+			'Huile Végétale Olive et Argan bio',
+			6.05,
+			12,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345678, 
+			'Huile Végétale de Baobab bio',
+			9.85,
+			8,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345679, 
+			'Huile Végétale Carotte bio',
+			4.95,
+			26,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345611, 
+			'Gel-crème hydratant anti-imperfections',
+			5.95,
+			25,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345612, 
+			'Gel nettoyant anti-imperfections',
+			5.10,
+			21,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345613, 
+			'Gel exfoliant anti-imperfections',
+			4.95,
+			14,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345614, 
+			'Masque charbon purifiant',
+			5.85,
+			18,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345615, 
+			'Masque Argile détoxifiant',
+			5.95,
+			21,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345616, 
+			'Crème hydratante bonne mine',
+			6.99,
+			13,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345617, 
+			'Crème anti-âge jour',
+			11.65,
+			8,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345618, 
+			'Gelée de nuit anti-âge',
+			8.00,
+			16,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345619, 
+			'Sérum fermeté anti-âge',
+			8.00,
+			16,
+			'Cosmétique');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345620, 
+			'Contour yeux et lèvres anti-âge',
+			8.10,
+			13,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345621, 
+			'Sérum éclat anti-âge Bio',
+			11.75,
+			9,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (12345622, 
+			'Sérum éclat anti-âge Bio',
+			11.75,
+			9,
+			'Cosmétique');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678910, 
+			'Gélule végétale mélisse relax',
+			5.20,
+			20,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678911, 
+			'Gélule végétale Stress Control bio',
+			6.30,
+			20,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678912, 
+			'Infusion Sommeil Réparateur bio',
+			1.95,
+			25,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678913, 
+			'Infusion Stress Control bio',
+			1.95,
+			24,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678914, 
+			'Compte gouttes Sommeil détente',
+			8.85,
+			15,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678915, 
+			'Gélules propolis immunité',
+			4.95,
+			18,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678916, 
+			'Gélules végétales Cheveux Ongles et Peau',
+			9.75,
+			12,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678917, 
+			'Gélules jambes légères Vigne rouge/cassis bio',
+			4.95,
+			18,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678918, 
+			'Thé tuocha Détox bio',
+			1.95,
+			25,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678919, 
+			'Thé Brûle Graisses bio',
+			1.95,
+			25,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678920, 
+			'Thé Aide Minceur bio',
+			1.95,
+			25,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678921, 
+			'Thé energy booster',
+			1.95,
+			25,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678922, 
+			'Infusion Draineur Intense bio',
+			2.05,
+			25,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678923, 
+			'Infusion Jambes légères bio',
+			2.05,
+			25,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678924, 
+			'Infusion Sommeil Réparateur bio',
+			2.05,
+			25,
+			'Bien-être');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (45678925, 
+			'Infusion Détox Foie bio',
+			2.05,
+			25,
+			'Bien-être');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912340, 
+			'Savon noir liquide à l huile de lin',
+			2.95,
+			20,
+			'Entretien');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912341, 
+			'Vinaigre ménager 12°',
+			1.75,
+			20,
+			'Entretien');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912342, 
+			'Vinaigre ménager framboise 12°',
+			1.95,
+			20,
+			'Entretien');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912343, 
+			'Bicarbonate de soude',
+			2.30,
+			20,
+			'Entretien');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912344, 
+			'Cristaux de soude',
+			2.30,
+			20,
+			'Entretien');
+			
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912345, 
+			'Acide citrique',
+			4.30,
+			20,
+			'Entretien');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912346, 
+			'Pierre d argile',
+			6.80,
+			12,
+			'Entretien');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912347, 
+			'Lessive poudre de savon bio',
+			6.95,
+			15,
+			'Entretien');
+
+INSERT INTO Produits(IdProd, NomProd, PrixHT, QuantiteStock, LibelleType)
+	VALUES (78912348, 
+			'Tablettes lave-vaisselle tout en un',
+			6.50,
+			17,
+			'Entretien');
+			
+INSERT INTO CodesPostaux(CP, Ville)
+	VALUES(17000, 'La Rochelle');
+
+INSERT INTO CodesPostaux(CP, Ville)
+	VALUES(17440, 'Aytré');
+
+INSERT INTO CodesPostaux(CP, Ville)
+	VALUES(17200, 'Royan');
+
+INSERT INTO CodesPostaux(CP, Ville)
+	VALUES(17300, 'Rochefort');
+
+INSERT INTO FraisDePort (PaysRel, FdP)
+	VALUES('France', 4.99);
+	
+INSERT INTO PointsRelais(IdRel, NomRel, NumRueRel, NomRueRel, CPRel, PaysRel)
+	VALUES (12345671, 
+			'Parashop', 
+			28, 
+			'Rue Des Merciers', 
+			17000, 
+			'France');
+
+INSERT INTO PointsRelais(IdRel, NomRel, NumRueRel, NomRueRel, CPRel, PaysRel)
+	VALUES (12345672, 
+			'COOP', 
+			44, 
+			'Rue Emile Normandin', 
+			17000, 
+			'France');
+
+INSERT INTO PointsRelais(IdRel, NomRel, NumRueRel, NomRueRel, CPRel, PaysRel)
+	VALUES (12345673, 
+			'Chantelle AYTRE', 
+			43, 
+			'Av Edmond Grasset', 
+			17440, 
+			'France');
+
+INSERT INTO PointsRelais(IdRel, NomRel, NumRueRel, NomRueRel, CPRel, PaysRel)
+	VALUES (12345674, 
+			'Le Jardin des Fleurs', 
+			2, 
+			'Boulevard Albert 1er', 
+			17200, 
+			'France');
+			
+INSERT INTO PointsRelais(IdRel, NomRel, NumRueRel, NomRueRel, CPRel, PaysRel)
+	VALUES (12345675, 
+			'Biomonde', 
+			9, 
+			'Av William Ponty', 
+			17300, 
+			'France');
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, CPCl)
+	VALUES (17925861, 
+			'Dupont', 
+			'Jean', 
+			84, 
+			'Rue du Moulin', 
+			'France', 
+			'dupont-jean@orange.fr', 
+			'0675684698', 
+			17300);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925862, 
+			'Tuen', 
+			'Valentine', 
+			58, 
+			'Allée des Cerisiers', 
+			'France', 
+			'valentine17@hotmail.fr', 
+			'0695759646', 
+			96,
+			TO_DATE('01/06/1989', 'DD/MM/YYYY'),
+			TO_DATE('25/08/2015', 'DD/MM/YYYY'),
+			17440);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925863, 
+			'Ligand', 
+			'Sebastien', 
+			156, 
+			'Allée du Petit Pont', 
+			'France', 
+			'sebastien_du_17@hotmail.fr', 
+			'0752395642',
+			135,
+			TO_DATE('24/04/1997', 'DD/MM/YYYY'),
+			TO_DATE('09/02/2022', 'DD/MM/YYYY'),
+			17200);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, CPCl)
+	VALUES (17925864, 
+			'Oudain', 
+			'Marie', 
+			462, 
+			'Place des Emmurés', 
+			'France', 
+			'marieoud@hotmail.fr', 
+			'0679912569', 
+			17440);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925865, 
+			'Sahraoui', 
+			'Melissa', 
+			189, 
+			'Rue de la Fontaine', 
+			'France', 
+			'melissasahraoui@gmail.com', 
+			'0658961548', 
+			20,
+			TO_DATE('17/01/1956', 'DD/MM/YYYY'),
+			TO_DATE('15/05/2020', 'DD/MM/YYYY'),			
+			17440);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, CPCl)
+	VALUES (17925866, 
+			'Hugo', 
+			'Jeanne', 
+			86, 
+			'Rue des pavés', 
+			'France', 
+			'jeanne.hugo@gmail.com', 
+			'0678963646', 
+			17000);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925867, 
+			'Descamp', 
+			'Georges', 
+			18, 
+			'Allée du Paradis', 
+			'France', 
+			'georges17440@gmail.com', 
+			'0678964631', 
+			58,
+			TO_DATE('28/07/1962', 'DD/MM/YYYY'),
+			TO_DATE('05/06/2022', 'DD/MM/YYYY'),			
+			17000);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925868, 
+			'Canu', 
+			'Anais', 
+			56, 
+			'Rue Georges Pompidou', 
+			'France', 
+			'anais.canu@orange.fr', 
+			'0678954862', 
+			96,
+			TO_DATE('15/09/1976', 'DD/MM/YYYY'),
+			TO_DATE('19/04/2021', 'DD/MM/YYYY'),			
+			17000);
+
+INSERT INTO Clients(IdCl, NomCl, PrenomCl, NumRueCl, NomRueCl, PaysCl, MailCl, TelCl, PointsFid, DateNaisCl, DateAdhCl, CPCl)
+	VALUES (17925869, 
+			'Vanger', 
+			'Carolle', 
+			52, 
+			'Allée du Cordonnier', 
+			'France', 
+			'Carolle_Vanger@laposte.com', 
+			'0759625963', 
+			142,
+			TO_DATE('19/07/2001', 'DD/MM/YYYY'),
+			TO_DATE('16/12/2022', 'DD/MM/YYYY'),			
+			17200);
+
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769432, 
+			'Doumé',
+			'Joachim');
+			
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769433, 
+			'Cardan',
+			'Joséphine');
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769434, 
+			'Hoyé',
+			'Karim');
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769435, 
+			'Toity',
+			'Anabelle');
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769436, 
+			'Valentin',
+			'Marie-Christine');
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769437, 
+			'Potin',
+			'Valérie');
+
+INSERT INTO Conseillers (IdCons, NomCons, PrenomCons)
+	VALUES (14769438, 
+			'Madim',
+			'Jean-Claude');
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695368,
+			TO_DATE('30/12/2022', 'DD/MM/YYYY'),
+			'En point relais',
+			TO_DATE('08/01/2023', 'DD/MM/YYYY'),
+			34.16,
+			17925867,
+			14769438,
+			12345671);
+			
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695368, 
+			12345674, 
+			1);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695368, 
+			12345615, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695368, 
+			45678921, 
+			5);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695369,
+			TO_DATE('04/04/2020', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('10/04/2020', 'DD/MM/YYYY'),
+			81.6,
+			17925865,
+			14769432,
+			12345673);
+			
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695369, 
+			78912346, 
+			10);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695370,
+			TO_DATE('08/01/2023', 'DD/MM/YYYY'),
+			'En préparation',
+			TO_DATE('18/01/2023', 'DD/MM/YYYY'),
+			32.16,
+			17925863,
+			14769435,
+			12345674);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695370, 
+			12345679, 
+			1);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695370, 
+			12345611, 
+			1);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695370, 
+			12345612, 
+			1);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695370, 
+			12345613, 
+			1);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695370, 
+			12345614, 
+			1);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695371,
+			TO_DATE('04/05/2019', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('16/05/2019', 'DD/MM/YYYY'),
+			33.12,
+			17925866,
+			14769436,
+			12345672);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695371, 
+			12345673, 
+			2);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695372,
+			TO_DATE('05/06/2015', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('15/06/2015', 'DD/MM/YYYY'),
+			41.92,
+			17925867,
+			14769434,
+			12345672);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695372, 
+			45678925, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695372, 
+			78912340, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695372, 
+			78912341, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695372, 
+			12345613, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695372, 
+			12345614, 
+			2);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695373,
+			TO_DATE('04/05/2019', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('16/05/2019', 'DD/MM/YYYY'),
+			0.18,
+			17925869,
+			14769435,
+			12345674);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695373, 
+			45678914, 
+			1);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695374,
+			TO_DATE('30/11/2021', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('10/12/2021', 'DD/MM/YYYY'),
+			26.24,
+			17925863,
+			14769436,
+			12345674);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695374, 
+			12345617, 
+			2);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695374, 
+			78912344, 
+			3);
+
+INSERT INTO Commandes(NumCmd, DateCmd, EtatCmd, DateLiv, PrixTotCmd, IdCl, IdCons, IdRel)
+	VALUES (14695375,
+			TO_DATE('08/07/2022', 'DD/MM/YYYY'),
+			'Finalisée',
+			TO_DATE('20/07/2022', 'DD/MM/YYYY'),
+			10.18,
+			17925867,
+			14769437,
+			12345671);
+
+INSERT INTO CompositionCmd(NumCmd, IdProd, QuantiteVoulue)
+	VALUES (14695375, 
+			45678920, 
+			9);
+
+INSERT INTO BonsAchats(NumBon, DateEmis, MontantBon, IdCl, NumCmd)
+	VALUES (78512586,
+			TO_DATE('06/05/2019', 'DD/MM/YYYY'),
+			10,
+			17925869,
+			14695373);
+
+INSERT INTO BonsAchats(NumBon, DateEmis, MontantBon, IdCl)
+	VALUES (78512587,
+			TO_DATE('01/01/2023', 'DD/MM/YYYY'),
+			10,
+			17925867);
+
+INSERT INTO BonsAchats(NumBon, DateEmis, MontantBon, IdCl)
+	VALUES (78512588,
+			TO_DATE('09/01/2023', 'DD/MM/YYYY'),
+			10,
+			17925863);
+
+INSERT INTO BonsAchats(NumBon, DateEmis, MontantBon, IdCl, NumCmd)
+	VALUES (78512589,
+			TO_DATE('02/12/2021', 'DD/MM/YYYY'),
+			10,
+			17925863,
+			14695374);
+
+INSERT INTO BonsAchats(NumBon, DateEmis, MontantBon, IdCl, NumCmd)
+	VALUES (78512590,
+			TO_DATE('09/07/2022', 'DD/MM/YYYY'),
+			10,
+			17925867,
+			14695375);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558739,
+			TO_DATE('01/01/2023', 'DD/MM/YYYY'),
+			14695368);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558740,
+			TO_DATE('06/04/2020', 'DD/MM/YYYY'),
+			14695369);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558741,
+			TO_DATE('09/01/2023', 'DD/MM/YYYY'),
+			14695370);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558742,
+			TO_DATE('06/05/2019', 'DD/MM/YYYY'),
+			14695371);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558743,
+			TO_DATE('06/06/2015', 'DD/MM/YYYY'),
+			14695372);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558744,
+			TO_DATE('06/05/2019', 'DD/MM/YYYY'),
+			14695373);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558745,
+			TO_DATE('02/12/2021', 'DD/MM/YYYY'),
+			14695374);
+
+INSERT INTO Factures (NumFac, DateFac, NumCmd)
+	VALUES (10558746,
+			TO_DATE('09/07/2022', 'DD/MM/YYYY'),
+			14695375);
+
+
+
